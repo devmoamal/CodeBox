f0f00a6d7012a4c55f0cb89862a983c5faa6d44b
Moamal Hussin <dev.moamal@gmail.com>
2026-04-02 19:06:08 +0300

    refactor(db): remove storage schema and types

--- server/src/db/types/storage.ts
+++
@@ -1,6 +0,0 @@
-import { InferInsertModel, InferSelectModel } from "drizzle-orm";
-import { storageTable } from "../schema/storage";
-
-export type Storage = InferSelectModel<typeof storageTable>;
-export type NewStorage = InferInsertModel<typeof storageTable>;
-export type UpdateStorage = Partial<NewStorage>;
