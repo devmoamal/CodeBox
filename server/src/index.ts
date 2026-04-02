bb4356a25c7d9d635a13ca76fdcd81648439153c
Moamal Hussin <dev.moamal@gmail.com>
2026-04-02 19:06:18 +0300

    feat(server): update entry point and routes index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import { app } from "@/app";
 import { env } from "@/config/env.config";
 import { logger } from "@/lib/logger";
+import { websocket } from "@/routes/api/terminal";
 
 export default {
   port: env.PORT,
   fetch: app.fetch,
+  websocket,
 };
 
 logger.info(`Server running on port ${env.PORT}`);
--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono";
 import storage from "./storage";
 import projects from "./projects";
+import runner from "./runner";
+import terminal from "./terminal";
 
 const router = new Hono();
 
 router.route("/projects", projects);
 router.route("/storage", storage);
+router.route("/runner", runner);
+router.route("/terminal", terminal);
 
 export default router;
